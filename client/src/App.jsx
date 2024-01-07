2a7a428813e724a35540f82c435e7df15ca2824c
afridkhan77 <afridhossain30@gmail.com>
2024-01-07 19:55:37 +0600

    u

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,61 +1,57 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 
 /** import all components */
-import Username from "./components/Username";
+import Username from './components/Username';
 import Password from './components/Password';
 import Register from './components/Register';
 import Profile from './components/Profile';
-import Reset from './components/Reset';
 import Recovery from './components/Recovery';
+import Reset from './components/Reset';
 import PageNotFound from './components/PageNotFound';
 
 
+/** auth middleware */
+import { AuthorizeUser, ProtectRoute } from './middleware/auth'
 
-// root routes
-const router = createBrowserRouter(
-  [
+/** root routes */
+const router = createBrowserRouter([
     {
-      path: '/',
-      element: <Username></Username>
+        path : '/',
+        element : <Username></Username>
     },
     {
-      path: '/register',
-      element:<Register></Register>
+        path : '/register',
+        element : <Register></Register>
     },
     {
-      path: '/password',
-      element:<Password></Password>
+        path : '/password',
+        element : <ProtectRoute><Password /></ProtectRoute>
     },
     {
-      path: '/profile',
-      element:<Profile></Profile>
+        path : '/profile',
+        element : <AuthorizeUser><Profile /></AuthorizeUser>
     },
     {
-      path: '/recovery',
-      element:<Recovery></Recovery>
+        path : '/recovery',
+        element : <Recovery></Recovery>
     },
     {
-      path: '/reset',
-      element:<Reset></Reset>
+        path : '/reset',
+        element : <Reset></Reset>
     },
     {
-      path: '*',
-      element: <PageNotFound></PageNotFound>
+        path : '*',
+        element : <PageNotFound></PageNotFound>
     },
-  ]
-)
+])
 
-function App() {
-  
-
+export default function App() {
   return (
-    
-   <main>
-      <RouterProvider router={router}></RouterProvider>
-   </main>
-    
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
   )
 }
-
-export default App;
 
