939fce641948740f66f231e4cbfabd764fcbe409
afridkhan77 <afridhossain30@gmail.com>
2024-01-07 19:49:18 +0600

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,116 +1,61 @@
-// import { createBrowserRouter,RouterProvider } from "react-router-dom";
-
-// /** import all components */
-// import Username from "./components/Username";
-// import Password from './components/Password';
-// import Register from './components/Register';
-// import Profile from './components/Profile';
-// import Reset from './components/Reset';
-// import Recovery from './components/Recovery';
-// import PageNotFound from './components/PageNotFound';
-
-
-
-// // root routes
-// const router = createBrowserRouter(
-//   [
-//     {
-//       path: '/',
-//       element: <Username></Username>
-//     },
-//     {
-//       path: '/register',
-//       element:<Register></Register>
-//     },
-//     {
-//       path: '/password',
-//       element:<Password></Password>
-//     },
-//     {
-//       path: '/profile',
-//       element:<Profile></Profile>
-//     },
-//     {
-//       path: '/recovery',
-//       element:<Recovery></Recovery>
-//     },
-//     {
-//       path: '/reset',
-//       element:<Reset></Reset>
-//     },
-//     {
-//       path: '*',
-//       element: <PageNotFound></PageNotFound>
-//     },
-//   ]
-// )
-
-// function App() {
-  
-
-//   return (
-    
-//    <main>
-//       <RouterProvider router={router}></RouterProvider>
-//    </main>
-    
-//   )
-// }
-
-// export default App;
-
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 
 /** import all components */
-import Username from './components/Username';
+import Username from "./components/Username";
 import Password from './components/Password';
 import Register from './components/Register';
 import Profile from './components/Profile';
-import Recovery from './components/Recovery';
 import Reset from './components/Reset';
+import Recovery from './components/Recovery';
 import PageNotFound from './components/PageNotFound';
 
 
 
+// root routes
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Username></Username>
+    },
+    {
+      path: '/register',
+      element:<Register></Register>
+    },
+    {
+      path: '/password',
+      element:<Password></Password>
+    },
+    {
+      path: '/profile',
+      element:<Profile></Profile>
+    },
+    {
+      path: '/recovery',
+      element:<Recovery></Recovery>
+    },
+    {
+      path: '/reset',
+      element:<Reset></Reset>
+    },
+    {
+      path: '*',
+      element: <PageNotFound></PageNotFound>
+    },
+  ]
+)
 
-/** root routes */
-const router = createBrowserRouter([
-    {
-        path : '/',
-        element : <Username></Username>
-    },
-    {
-        path : '/register',
-        element : <Register></Register>
-    },
-    {
-        path : '/password',
-        element : <Password />
-    },
-    {
-        path : '/profile',
-        element : <Profile />
-    },
-    {
-        path : '/recovery',
-        element : <Recovery></Recovery>
-    },
-    {
-        path : '/reset',
-        element : <Reset></Reset>
-    },
-    {
-        path : '*',
-        element : <PageNotFound></PageNotFound>
-    },
-])
+function App() {
+  
 
-export default function App() {
   return (
-    <main>
-        <RouterProvider router={router}></RouterProvider>
-    </main>
+    
+   <main>
+      <RouterProvider router={router}></RouterProvider>
+   </main>
+    
   )
 }
+
+export default App;
+
--- client/src/components/Username.jsx
+++ client/src/components/Username.jsx
@@ -2,16 +2,7 @@ import React from 'react';
 
 const Username = () => {
     return (
-       <div className="div container mx-auto">
-        <div className='flex justify-center items-center h-screen'>
-            <div>
-                <div className="title flex flex-col items-center">
-                    <h4>hello again</h4>
-                </div>
-            </div>
-
-        </div>
-       </div>
+        
     );
 };
 
